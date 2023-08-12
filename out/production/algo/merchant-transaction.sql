ff7613814c881d7fd670adf3020e38b511ba2a59
chukwuebuka <chukwuebuka.anazodo@yenlo.com>
2023-08-12 21:27:23 +0200

    updates to algo

--- out/production/algo/merchant-transaction.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT UPPER(m.merchantName), t.hashedShopperReference, ROUND(AVG(riskScore), 2) as averageRiskScore, COUNT(*) as totalNumberOfTransactions
-FROM transaction t
-INNER JOIN merchant m ON t.merchantId = m.merchantId
-GROUP BY t.hashedShopperReference, m.merchantName
-HAVING AVG(riskScore) >= 100
-ORDER BY m.merchantName ASC, AVG(riskScore) DESC;
--- src/MySolution.java
+++
@@ -1,3 +0,0 @@
-public class MySolution {
-//    p
-}
